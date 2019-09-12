b342d0d0026315a17290066f9db31470b238a375
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-12 02:09:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url)).css(".student-card")
     doc.map do |info| 
-      
-
+      name = info.css("h4.student-name").text
+      location = info.css("p.student-location").text
+      link = info.css("a @href").text
+      {:name => name, :location => location, :profile_url => link}
+    end
+  end
   end
 
   def self.scrape_profile_page(profile_url)
