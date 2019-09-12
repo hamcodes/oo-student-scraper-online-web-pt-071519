b973d3aa138aca5859ac48d34bffa4f182ff5fe9
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-12 02:23:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,9 +16,26 @@ class Scraper
   
 
   def self.scrape_profile_page(profile_url)
-        hash = {}
-        doc = Nokogiri::HTML(open(profile_url))
-  end
+    hash = {}
+    doc = Nokogiri::HTML(open(profile_url))
+    links = doc.css("div.social-icon-container a").collect{|s| s["href"]}
+    links.each do |link|     
+      if link.include?("twitter")
+        hash[:twitter] = link
+      elsif link.include?("linkedin")
+        hash[:linkedin] = link 
+      elsif link.include?("github")
+        hash[:github] = link 
+      elsif link.include?(".com")
+        hash[:blog] = link 
+        end
+      end
+      hash[:bio] =  doc.css("div.bio-content.content-holder").css("div.description-holder").text.strip
+      hash[:profile_quote] = doc.css(".profile-quote").text.strip
+      hash
+    end
+
+end
 
 end
 
